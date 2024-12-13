db7c60826a6bfc17746c3dd666666427848fb672
itsMe-Manar <147439334+itsMe-Manar@users.noreply.github.com>
2024-12-13 13:36:38 +0100

    Delete src directory

--- src/command.rs
+++
@@ -1,8 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-pub enum TodoCommand {
-    AddTask { task_id: String, description: String },
-    CompleteTask { task_id: String },
-    DeleteTask { task_id: String },
-}
--- src/src/state.rs
+++
@@ -1,6 +0,0 @@
-use crate::aggregate::TaskList;
-use std::{sync::Arc};
-use tokio::sync::Mutex;
-
-// Shared state type alias
-pub type SharedState = Arc<Mutex<TaskList>>;
